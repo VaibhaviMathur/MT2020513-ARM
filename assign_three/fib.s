1ee02f9cd84f7e59cddd0d7c2af501bebcc1a818
VaibhaviMathur <88839318+VaibhaviMathur@users.noreply.github.com>
2021-10-31 22:13:50 +0530

    Update fib.s

--- assign_three/fib.s
+++ assign_three/fib.s
@@ -1,29 +1,25 @@
-     AREA     factorial, CODE, READONLY
+         AREA     factorial, CODE, READONLY
      EXPORT __main
 	 IMPORT printMsg
 	 IMPORT printMsg2p
 	 IMPORT printMsg4p
 	 ENTRY 
 __main  FUNCTION	
-	  MOV R3,#0x20000000
-	  MOV R1,#0x0
-	  MOV R2,#0x1
+	  MOV R11,#0x20000000
+	  MOV R10,#0x0
+	  MOV R9,#0x1
 	  MOV R8,#0x0
-l1   STR R1,[R3]           ;Series is stored in array whose base address is in R3
-      ADD R3,R3,#0x5
+l1    STR R10,[R11]           ;Series is stored in array whose base address is in R3
+      MOV R0,R10
+	  BL printMsg
+   	  ADD R11,R11,#0x5
 	  ADD R8,R8,#0x1
 	  CMP R8,#0xA
-	  BEQ l2
-	  ADD R4,R1,R2
-	  MOV R1,R2
-	  MOV R2,R4
+	  BEQ stop
+	  ADD R4,R10,R9
+	  MOV R10,R9
+	  MOV R9,R4
 	  B l1
-l2    BL printMsg
-	 
-	 
-      
-	
-		
 
 stop    B stop ; stop program
      ENDFUNC
